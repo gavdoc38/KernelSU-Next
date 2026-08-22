af6bd1562f8a44c6a3e02aea1ea3dd28bae9edbe
gavdov38 <gavin.a.docherty@gmail.com>
2026-08-22 07:42:49 +0100

    Remove KSU_FEATURE_AVC_SPOOF as susfs covers this

--- kernel/runtime/boot_event.c
+++ kernel/runtime/boot_event.c
@@ -14,8 +14,6 @@ bool ksu_module_mounted __read_mostly = false;
 bool ksu_boot_completed __read_mostly = false;
 extern void stop_input_hook();
 
-extern void ksu_avc_spoof_late_init();
-
 void on_post_fs_data(void)
 {
 	static bool done = false;
@@ -67,5 +65,4 @@ void on_boot_completed(void)
     ksu_boot_completed = true;
     pr_info("on_boot_completed!\n");
     track_throne(true);
-    ksu_avc_spoof_late_init();
 }
